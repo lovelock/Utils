543c5a83c816864d811b6030caa4cff1f3f10714
lovelock <frostwong@gmail.com>
2015-01-09 11:53:24 +0800

    add cookie tools

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Utils
+
+Sometimes I have to solve similar problems, so I record these snippets for future use.
---
+++ js/cookies.js
@@ -0,0 +1,7 @@
+var Cookies = {
+    getCookie: function (name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+    }
+}
