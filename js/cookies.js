f6c7e855cf691b828829c020a59eeb6373dfe45d
lovelock <frostwong@gmail.com>
2015-01-09 16:20:38 +0800

    simple cookie tools

--- js/cookies.js
+++ js/cookies.js
@@ -1,7 +1,23 @@
 var Cookies = {
-    getCookie: function (name) {
+    createCookie: function (name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        }
+        else {
+            var expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    },
+    readCookie: function (name) {
         var value = "; " + document.cookie;
         var parts = value.split("; " + name + "=");
-        if (parts.length == 2) return parts.pop().split(";").shift();
+        if (parts.length == 2)  {
+            return parts.pop().split(";").shift();
+        }
+    },
+    eraseCookie: function (name) {
+        createCookie(name,"",-1);
     }
 }
