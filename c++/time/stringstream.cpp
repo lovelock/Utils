6f13f93c1a56b5accc363bb4be664e51d842538a
lovelock <frostwong@gmail.com>
2015-03-19 20:13:25 +0800

    remove comments

--- c++/time/stringstream.cpp
+++ c++/time/stringstream.cpp
@@ -6,13 +6,6 @@
 
 int main()
 {
-    /*
-     * std::stringstream ss;
-     * ss << seconds;
-     * std::string ts = ss.str();
-     * std::cout << ss << std::endl;
-     */
-
     std::time_t seconds = std::time(NULL);
     std::string ts = boost::lexical_cast<std::string>(seconds);
     std::cout << ts << std::endl;
